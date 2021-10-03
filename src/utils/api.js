6cd0a2fc3cc14e4062cd67256bbbfa48930887ab
olga <oluciams84@gmail.com>
2021-10-03 12:17:20 -0500

    se cambia URL para trabajar localmente modificaciones

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,22 +1,22 @@
 import axios from "axios";
 
 export const loginApi = axios.create({
-    //baseURL: 'http://localhost:3001',
+    baseURL: 'http://localhost:3001',
     //baseURL: 'https://ancient-brook-80759.herokuapp.com',
-    baseURL: process.env.REACT_APP_API_URL,
+    //baseURL: process.env.REACT_APP_API_URL,
     timeout: 5000,
 });
 
 export const productsApi= axios.create({
-    //baseURL: 'http://localhost:3001',
+    baseURL: 'http://localhost:3001',
     //baseURL:'https://ancient-brook-80759.herokuapp.com',
-    baseURL: process.env.REACT_APP_API_URL,
+    //baseURL: process.env.REACT_APP_API_URL,
     timeout:5000
 })
 
 export const signupApi= axios.create({
-    //baseURL: 'http://localhost:3001',
+    baseURL: 'http://localhost:3001',
     //baseURL:'https://ancient-brook-80759.herokuapp.com',
-    baseURL: process.env.REACT_APP_API_URL,
+    //baseURL: process.env.REACT_APP_API_URL,
     timeout:5000
 })
