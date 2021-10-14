02ef221c10e9586117e1aad1b56263960e4c2c04
olga <oluciams84@gmail.com>
2021-10-14 17:19:55 -0500

    URL de la Api

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,28 +1,54 @@
 import axios from "axios";
 
-export const loginApi = axios.create({   
-    //baseURL: 'http://localhost:3001',
-    baseURL: 'https://ancient-brook-80759.herokuapp.com',
+// export const loginApi = axios.create({   
+//     //baseURL: 'http://localhost:3001',
+//     baseURL: 'https://ancient-brook-80759.herokuapp.com',
+//     timeout: 5000,
+// });
+
+// export const productsApi= axios.create({
+//     //baseURL: 'http://localhost:3001',
+//     baseURL:'https://ancient-brook-80759.herokuapp.com',
+//     timeout:5000
+// })
+
+// export const signupApi= axios.create({    
+//     //baseURL: 'http://localhost:3001',   
+//     baseURL:'https://ancient-brook-80759.herokuapp.com',
+//     timeout:5000
+// })
+
+// export const cloudinaryApi= axios.create({    
+//     //baseURL: 'http://localhost:3001',   
+//     baseURL:'https://ancient-brook-80759.herokuapp.com',
+//     timeout:2000
+// })
+
+export const loginApi = axios.create({
+    baseURL: 'http://localhost:3001',
+    //baseURL: 'https://ancient-brook-80759.herokuapp.com',
+    //baseURL: process.env.REACT_APP_API_URL,
     timeout: 5000,
 });
 
 export const productsApi= axios.create({
-    //baseURL: 'http://localhost:3001',
-    baseURL:'https://ancient-brook-80759.herokuapp.com',
+    baseURL: 'http://localhost:3001',
+    //baseURL:'https://ancient-brook-80759.herokuapp.com',
+    //baseURL: process.env.REACT_APP_API_URL,
     timeout:5000
 })
 
-export const signupApi= axios.create({    
-    //baseURL: 'http://localhost:3001',   
-    baseURL:'https://ancient-brook-80759.herokuapp.com',
+export const signupApi= axios.create({
+    baseURL: 'http://localhost:3001',
+    //baseURL:'https://ancient-brook-80759.herokuapp.com',
+    //baseURL: process.env.REACT_APP_API_URL,
     timeout:5000
 })
 
-export const cloudinaryApi= axios.create({    
-    //baseURL: 'http://localhost:3001',   
-    baseURL:'https://ancient-brook-80759.herokuapp.com',
-    timeout:2000
-})
+
+
+
+
 
 
 
