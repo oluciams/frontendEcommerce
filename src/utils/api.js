56eac667d6bf82eac6087424b1c863f94d3bb222
olga <oluciams84@gmail.com>
2021-10-16 10:57:28 -0500

    Adiciona cambios en las variables de entorno para pruebas locales

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,48 +1,27 @@
 import axios from "axios";
 
-// export const loginApi = axios.create({   
-//     //baseURL: 'http://localhost:3001',
-//     baseURL: 'https://ancient-brook-80759.herokuapp.com',
-//     timeout: 5000,
-// });
-
-// export const productsApi= axios.create({
-//     //baseURL: 'http://localhost:3001',
-//     baseURL:'https://ancient-brook-80759.herokuapp.com',
-//     timeout:5000
-// })
-
-// export const signupApi= axios.create({    
-//     //baseURL: 'http://localhost:3001',   
-//     baseURL:'https://ancient-brook-80759.herokuapp.com',
-//     timeout:5000
-// })
-
-// export const cloudinaryApi= axios.create({    
-//     //baseURL: 'http://localhost:3001',   
-//     baseURL:'https://ancient-brook-80759.herokuapp.com',
-//     timeout:2000
-// })
-
 export const loginApi = axios.create({
     baseURL: 'http://localhost:3001',
-    //baseURL: 'https://ancient-brook-80759.herokuapp.com',
     //baseURL: process.env.REACT_APP_API_URL,
     timeout: 5000,
 });
 
 export const productsApi= axios.create({
-    baseURL: 'http://localhost:3001',
-    //baseURL:'https://ancient-brook-80759.herokuapp.com',
+    baseURL: 'http://localhost:3001',    
     //baseURL: process.env.REACT_APP_API_URL,
     timeout:5000
 })
 
 export const signupApi= axios.create({
     baseURL: 'http://localhost:3001',
-    //baseURL:'https://ancient-brook-80759.herokuapp.com',
     //baseURL: process.env.REACT_APP_API_URL,
     timeout:5000
+})
+
+export const cloudinaryApi= axios.create({    
+    baseURL: 'http://localhost:3001',   
+    //baseURL: process.env.REACT_APP_API_URL,
+    timeout:2000
 })
 
 
