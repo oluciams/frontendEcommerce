5c89b5277ef4c649f5da5b00dad8c66bd56bd695
olga <oluciams84@gmail.com>
2022-10-30 16:26:13 -0500

    ajustes al codigo

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -19,24 +19,3 @@ export const Modal = ({children, isOpen, closeModal})=>{
 
   )
 }
-
-// export const Modal = ()=>{
-
-//   return (
-//   <Modal.Dialog>
-//       <Modal.Header closeButton>
-//       <Modal.Title>Modal title</Modal.Title>
-//     </Modal.Header>
-
-//     <Modal.Body>
-//       <p>Modal body text goes here.</p>
-//     </Modal.Body>
-
-//     <Modal.Footer>
-//       <Button variant="secondary">Close</Button>      
-//     </Modal.Footer>
-//   </Modal.Dialog>
-  
-  
-//   )
-// }
